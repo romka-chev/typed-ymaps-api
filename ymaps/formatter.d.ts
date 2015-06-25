dcf537743c952e9c77c5c602af47a904eff22434
Kulikov Roman <flinnraider@yandex.ru>
2015-06-25 18:46:27 +0300

    added formatter

---
+++ ymaps/formatter.d.ts
@@ -0,0 +1,6 @@
+module ymaps {
+    export class formatter {
+        distance(value:number, significantDigits:number = 2):string;
+        duration(value:number, significantDigits:number = 2):string;
+    }
+}
