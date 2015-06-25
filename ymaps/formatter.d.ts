f9c2dd00e522408a77ecdf20f41f9275d5963da8
Kulikov Roman <flinnraider@yandex.ru>
2015-06-25 18:48:08 +0300

    fixed static

--- ymaps/formatter.d.ts
+++ ymaps/formatter.d.ts
@@ -1,6 +1,6 @@
 module ymaps {
     export class formatter {
-        distance(value:number, significantDigits:number = 2):string;
-        duration(value:number, significantDigits:number = 2):string;
+        static distance(value:number, significantDigits:number = 2):string;
+        static duration(value:number, significantDigits:number = 2):string;
     }
 }
