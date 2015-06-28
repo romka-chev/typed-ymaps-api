cd37e14138ec021d4262f495ac73dfee079b1a3b
Kulikov Roman <flinnraider@yandex.ru>
2015-06-28 15:02:29 +0300

    added meta

---
+++ ymaps/meta.d.ts
@@ -0,0 +1,10 @@
+module ymaps {
+    export var meta:{
+        coordinatesOrder:"latlong"|"longlat";
+        countryCode:string;
+        languageCode:string;
+        mode:"release"|"debug";
+        ns:any;
+        version:string;
+    }
+}
