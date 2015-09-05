a602a5995057a795816b5a97d1e4c3925e5ad472
Kulikov Roman <flinnraider@yandex.ru>
2015-09-05 20:32:13 +0300

    updated package interactivityModel

--- ymaps/interactivityModel/storage.d.ts
+++ ymaps/interactivityModel/storage.d.ts
@@ -1,9 +1,3 @@
-module ymaps {
-    export module interactivityModel {
-        export class storage {
-            static add(key:string, object:any):util.Storage;
-            static get(key:string):any;
-            static remove(key:string):util.Storage;
-        }
-    }
+namespace ymaps.interactivityModel {
+    declare var storage:util.Storage;
 }
