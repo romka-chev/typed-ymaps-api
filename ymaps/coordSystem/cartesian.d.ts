de0e5b1d4626b127c6ecbf6808615ef4e3a5ecac
Kulikov Roman <flinnraider@yandex.ru>
2015-09-05 17:35:33 +0300

    updated coordSystem package

--- ymaps/coordSystem/cartesian.d.ts
+++ ymaps/coordSystem/cartesian.d.ts
@@ -1,7 +1,12 @@
-module ymaps {
-    export module coordSystem {
-        export class cartesian extends interfaces.ICoordSystem {
-            static scale:number|number[] = 1;
-        }
+namespace ymaps.coordSystem {
+
+    // todo check is this constructions correct
+    interface CartesianCoordSystemStatic {
+        new(scale:number|number[] = 1):CartesianCoordSystem;
+        (scale:number|number[] = 1):CartesianCoordSystem;
     }
+    interface CartesianCoordSystem extends ymaps.interfaces.ICoordSystem {
+    }
+
+    declare var cartesian:CartesianCoordSystemStatic;
 }
--- ymaps/coordSystem/geo.d.ts
+++ ymaps/coordSystem/geo.d.ts
@@ -1,7 +1,12 @@
-module ymaps {
-    export module coordSystem {
-        export class geo extends interfaces.ICoordSystem {
+namespace ymaps.coordSystem {
 
-        }
+    // todo check is this constructions correct
+    interface GeoCoordSystemStatic {
+        new(scale:number|number[] = 1):GeoCoordSystem;
+        (scale:number|number[] = 1):GeoCoordSystem;
     }
+    interface GeoCoordSystem extends ymaps.interfaces.ICoordSystem {
+    }
+
+    declare var geo:GeoCoordSystemStatic;
 }
